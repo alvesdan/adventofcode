c2d8d859f97eae819db7df9cf785484f1eff46b6
Daniel Alves <daniel@danielalves.me>
2015-12-23 10:27:35 +0100

    Update day 1

--- 01/solver.ex
+++ 01/solver.ex
@@ -1,11 +1,12 @@
 defmodule SolverOne do
-  def solve(input) do
-    s = String.split(input, "")
-    Enum.reduce(s, 0, fn(char, acc) ->
+  def solve do
+    {:ok, input} = File.read("input.txt")
+    list = String.split(input, "")
+    Enum.reduce(list, 0, fn(char, acc) ->
       case char do
         "(" -> acc + 1
         ")" -> acc - 1
-        _ -> acc
+        _   -> acc
       end
     end)
   end
