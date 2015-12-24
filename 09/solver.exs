f3b6d454631c96206e9384b6d7ae9a3e24568ad5
Daniel Alves <daniel@danielalves.me>
2015-12-24 14:35:34 +0100

    Finish day 9

--- 09/solver.exs
+++ 09/solver.exs
@@ -2,52 +2,46 @@ defmodule SolverNine do
   def solve do
     {:ok, input} = File.read("input.txt")
     lines = String.split(input, "\n", trim: true)
-    distances = parse(lines)
-    start = sort(distances) |> List.first
-    goto(start, tl(distances), 0)
+    dis = distances(lines)
+
+    Enum.reduce(permutations(cities(lines)), [], fn(route, acc) ->
+      distance = Enum.reduce(route, [0, 0], fn(a, acc) ->
+        [index, total] = acc
+        b = Enum.at(route, index + 1)
+        case b do
+          nil -> [index + 1, total]
+          _   -> [index + 1, total + (dis[{a, b}] || dis[{b, a}])]
+        end
+      end) |> List.last
+
+      Enum.concat(acc, [distance])
+    end) |> Enum.max
   end
 
-  defp parse(lines) do
+  defp cities(lines) do
     Enum.reduce(lines, [], fn(line, acc) ->
       case Regex.run(~r/(\w+) to (\w+) = (\d+)/, line) do
         [_, a, b, distance] ->
-          Enum.concat(acc, [
-            [a <> b, distance: String.to_integer(distance)],
-            [b <> a, distance: String.to_integer(distance)]
-          ])
+          Enum.uniq(Enum.concat(acc, [a,b]))
         _ -> acc
       end
     end)
   end
 
-  defp sort(distances) do
-    Enum.sort_by(distances, fn(dis) ->
-      dis[:distance]
+  def distances(lines) do
+    Enum.reduce(lines, %{}, fn(line, acc) ->
+      case Regex.run(~r/(\w+) to (\w+) = (\d+)/, line) do
+        [_, a, b, distance] ->
+          Dict.put acc, {a, b}, String.to_integer(distance)
+          Dict.put acc, {b, a}, String.to_integer(distance)
+        _ -> acc
+      end
     end)
   end
 
-  defp goto(current, [], covered) do
-    covered
-  end
-
-  defp goto(current, distances, covered) do
-    IO.inspect(current)
-    destiny = current[:b]
-    distance = current[:distance]
-    possible = filter(current, destiny, distances)
-    next = sort(possible) |> List.first
-
-    goto(
-      next,
-      List.delete(distances, next),
-      covered + distance
-    )
-  end
-
-  defp filter(current, destiny, distances) do
-    Enum.filter(distances, fn(dis) ->
-      dis[:a] == destiny
-    end)
+  def permutations([]), do: [[]]
+  def permutations(list) do
+    for h <- list, t <- permutations(list -- [h]), do: [h | t]
   end
 end
 
